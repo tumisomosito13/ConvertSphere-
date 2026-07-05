32eabd6a3761d34ae41c49fae90893995761a9d0
tumisomosito13 <tumisomosito13@gmail.com>
2026-07-05 18:28:34 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// ConvertSphere Configuration
+
+const CONFIG = {
+  API_URL: "https://convertsphere-api.tumisomosito13.workers.dev",
+  APP_NAME: "ConvertSphere",
+  VERSION: "1.0.0"
+};
+
+console.log("Connected to:", CONFIG.API_URL);
